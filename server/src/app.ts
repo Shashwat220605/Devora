45866b2b13675de9eb5fdb4fd6edcb68ad7a5513
Shashwat Ghadge <shxshwat23@gmail.com>
2026-08-22 15:10:14 +0530

    Mount explicit GitHub OAuth URL route

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,7 @@ import projectRoutes from "./routes/project.routes.js";
 import githubRoutes from "./routes/github.routes.js";
 import githubRepoRoutes from "./routes/github-repo.routes.js";
 import githubOAuthRoutes from "./routes/github-oauth.routes.js";
+import githubOAuthUrlRoutes from "./routes/github-oauth-url.routes.js";
 import githubDiffRoutes from "./routes/github-diff.routes.js";
 import githubSyncRoutes from "./routes/github-sync.routes.js";
 
@@ -51,6 +52,7 @@ app.use("/api/projects", projectRoutes);
 app.use("/api", githubRoutes);
 app.use("/api", githubRepoRoutes);
 app.use("/api", githubOAuthRoutes);
+app.use("/api", githubOAuthUrlRoutes);
 app.use("/api", githubDiffRoutes);
 app.use("/api", githubSyncRoutes);
 
