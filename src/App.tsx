2d08c95f000b79374d2b32a4333ff0d7f6bc121c
Shashwat Ghadge <shxshwat23@gmail.com>
2026-08-24 22:00:13 +0530

    Add Deployment Center route

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import AICodeActions from "./pages/AICodeActions";
 import AIChat from "./pages/AIChat";
 import CodeRunner from "./pages/CodeRunner";
 import SourceControl from "./pages/SourceControl";
+import DeploymentCenter from "./pages/DeploymentCenter";
 import Profile from "./pages/Profile";
 import { ActivityPage, SettingsPage, TerminalPage } from "./pages/ToolPages";
 
@@ -36,6 +37,7 @@ export default function App() {
     <Route path="/github/diff" element={<Protected><GitHubDiffViewer /></Protected>} />
     <Route path="/github/prs" element={<Protected><PullRequestCenter /></Protected>} />
     <Route path="/source-control" element={<Protected><SourceControl /></Protected>} />
+    <Route path="/deployments" element={<Protected><DeploymentCenter /></Protected>} />
     <Route path="/runner" element={<Protected><CodeRunner /></Protected>} />
     <Route path="/terminal" element={<Protected><TerminalPage /></Protected>} />
     <Route path="/activity" element={<Protected><ActivityPage /></Protected>} />
